d30ec9f7925af0542c94873c942c26989475ac76
Soubhik Barari <soubhikbarari@gmail.com>
2014-11-19 23:10:54 -0500

    updated pagerank

--- pagerank.m
+++ pagerank.m
@@ -26,13 +26,12 @@ for i=1:N,
 end
 
 % (1) MATLAB eig method
-tol = 0.00002;
+tol = 0.00001;
 [eigvec,eigval] = eig(ptG);
 [deig, col] = max(max(abs(eigval)));
 if (eigval(1) <= 1 + tol) && (eigval(1) >= 1 - tol)
-    sp0 = eigvec(1,:);
+    sp0 = eigvec(:,1);
     sp0 = abs(sp0) / sum(abs(sp0));
-    sp0 = transpose(sp0);
 else
     sp0 = eigvec(col,:);
     sp0 = transpose(sp0);
@@ -42,7 +41,7 @@ end
 % (2) Power iteration method
 sp1 = zeros(N,1);
 sp1(1) = 1;
-tol = 0.0000002;
+tol = 0.0000001;
 n = [0];
 while(1)
     iter = ptG * sp1;
@@ -65,4 +64,15 @@ function pr = tran_prob(p, d, N)
 %   using random surfer model
 
 pr = d*p + (1 - d)*(1/N);
+end
+
+
+function [t_rank, t_index] = report_ten_urls(r, u)
+% report ranks / indices of top 10 ranking urls
+% from pagerank
+[t_rank, t_index] = sort(r, 'descend')
+for i=1:10
+    u(t_index(i))
+    r(t_index(i))
+end
 end
